ece32db24ed816b328c931ad9d64fb33c127ca07
Thomas Hoos <thoos@cps.edu>
2017-06-11 20:10:12 +0000

    added new todos

--- index.js
+++ index.js
@@ -2,4 +2,18 @@ $(document).ready(function() {
     $("ul").on("click", "input[type=checkbox]",function(){
      $(this).closest("li").toggleClass("completed-todo")
     })
+
+    $("#add-new-todo-button").click(function(){
+   var todoBlock = $(this).closest("#new-todo-block")
+   var description = todoBlock.find("#new-todo-description")
+   var pomodoroEstimate = todoBlock.find("#new-pomodoro-estimate")
+
+   // Append the new todo to the list of todos
+   $("ul").append("<li> <input type='checkbox'/> " + description.val() + " <span class='pomodoro-estimate'>" + pomodoroEstimate.val() + " pomodoros</span> </li>")
+
+   // Clear the input fields for new-todo-description and pomdoro-estimate by entering an empty string as the value
+   description.val("")
+   pomodoroEstimate.val("")
+ })
 })
+    
